d5d713d2b171bb0605d7f44ec7ce979eedae5d24
Bingxi Du <bxdu@Bingxis-MacBook-Pro.local>
2018-10-30 00:56:17 -0400

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,6 +52,13 @@ $.ajax({
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //Basic functions will be called
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//creat a function that change the background image
+function changeBackground(){
+  $("body").css("background-image","url('assets/images/Qbackground.jpg'")
+  console.log("background should change")
+}
+
+
 
 //creat function used to generate random array of length without repetting, also including a;
 function shuffle(array) {
@@ -134,8 +141,9 @@ function randomGif(){
     console.log(key);
     console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     var playerGif = $("<img>").attr("src", gifList[key]);
-    playerGif.css("width","30%")
-    playerGif.css("margin","35%")
+    playerGif.css("width","40%")
+    playerGif.css("margin-left","30%")
+    playerGif.css("margin-top","30px")
     $(".gif").append(playerGif);
     console.log("The name of that player should be")
     console.log(playerList[key]);
@@ -176,7 +184,7 @@ for (i=0;i<4;i++){
   }
   if(choice == 2){
    answerText = positionList[n];
- }
+  }
 
   var answerButn = $("<button>");
 
@@ -211,202 +219,148 @@ function buttonValue(){
 }
 
 
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Set Time Out Function
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++= 
+//Creat a timeout function to negative page if time out
+//Creat a timeout function on the result page to question page
+var qTimeout;
+var rTimeout;
+function changePage(R){
+  if ( R==1 ){
+    startButtn();
+    clearTimeout(rTimeout);
+  }
+  if ( R==0 ){
+    resultPage(0);
+    clearTimeout(qTimeout);
+  }
+}
+
 //Creat a function that when click on button start 
+
+
 function startButtn(){
+  changeBackground();
     quesSelector();
     randomGif();
     answerSection();
     $("h1").remove();
     $("h3").remove();
+    qTimeout = setTimeout(changePage(0),5000);
 }
 
-  $("#start").click(startButtn);
-  $("button").click(clickonButn);
-  $(".answerButton").click(buttonValue);
+ 
 
- });
+ //Creat a function to show a result page 
+ function resultPage(R){
 
+  $(".question, .gif").empty();
+  $("#buttons").empty();
+  $("#timer").empty();
+  $("body").css("background-image","url('assets/images/Rbackground')")
+  var resultGif = $("<img>");
+  var resultText = $("<p>");
+  //the following if statements changes gif for different results
+   if (R==1){ 
+    resultGif.attr("src","https://gfycat.com/gifs/detail/accomplishedwillinggrouper");
+    resultText.text("SCORED")
+   }
 
+   if (R==0){ 
+    resultGif.attr("src","https://giphy.com/gifs/life-minute-tough-x6idyrAkU722Q");
+    resultText.text("YOU DROPPED BALL")
+   }
+
+   $(".question").append(resultText);
+   $(".gif").append(resultGif);
+
+   qTimeout = setTimeout(changePage(1),2000);
+
+ }
+//creat a function to tell if the answert is creat 
+function trueOrnot(){
+  var value = this.attr("data-answer");
   
+}
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Stop Watch
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //  Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
-var intervalTimber;
 // prevents the clock from being sped up unnecessarily
 var clockRunning = false;
-var timerRunning = false;
-
-
-//timer object.
-
-// var timer = {
-
-//   time: 0,
-
-//   start: function() {
-
-//     //  TODO: Use setInterval to start the count here and set the clock to running.
-    
-//     if (!timerRunning) {
-//       intervalTimber = setInterval(timer.count, 1000);
-//       timerRunning = true;
-//     }
-
-//   },
-
-//   stop: function() {
-//       clearInterval(intervalTimber);
-//     //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
-//   },
-
-
-//    count: function() {
-//     //  TODO: increment time by 1
-//     timer.time++;
-//     var  timeRemain = timer.timeCountdown(timer.time);
-//     $("#timer").text(timeRemain);
-
-//   },
-
-//   //  THIS FUNCTION IS DONE FOR US!
-//   //  We do not need to touch it.
-
-//   timeCountdown: function(t) {
-
-//     //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-//     var minutes = 29- Math.floor(t / 60);
-//     var seconds = 60- t%60;
-//     console.log(seconds)
-    
-//     if (seconds < 10) {
-//       seconds = "0" + seconds;
-//     }
-
-//     if (t/60 == 0) {
-//       seconds = "00";
-//     }
-
-//     if (minutes < 10) {
-//       minutes = "0" + minutes;
-//     }
-
-//     return minutes + ":" + seconds;
-//   }
-
-// };
-
-
 
 
 //  Our stopwatch object.
-// var stopwatch = {
+var stopwatch = {
 
-//   time: 0,
-//   lap: 1,
+  time: 0,
 
-//   reset: function() {
+  reset: function() {
 
-//     stopwatch.time = 0;
-//     stopwatch.lap = 1;
+    stopwatch.time = 0;
 
-//     $("#display").text('00:00');
+    $("#timer").text('00:05');
 
-//   },
+  },
 
-//   start: function() {
+  start: function() {
 
-//     //  TODO: Use setInterval to start the count here and set the clock to running.
+    //  TODO: Use setInterval to start the count here and set the clock to running.
     
-//     if (!clockRunning) {
-//       intervalId = setInterval(stopwatch.count, 1000);
-//       clockRunning = true;
-//       console.log("Started")
-//       console.log(intervalId)
-//     }
+    if (!clockRunning) {
+      intervalId = setInterval(stopwatch.count, 1000);
+      clockRunning = true;
+      console.log("Started")
+      console.log(intervalId)
+    }
 
-//   },
+  },
 
-//   stop: function() {
-//       clearInterval(intervalId);
-//       console.log("stopped")
-//       console.log(intervalId)
+  stop: function() {
+      clearInterval(intervalId);
+      console.log("stopped")
+      console.log(intervalId)
 
-//     //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
+    //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
 
-//   },
+  },
 
-//   recordLap: function() {
-
-//     //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
-//     //        and save the result in a variable.
-//     var timeCurrent = stopwatch.timeConverter(stopwatch.time)
-//     //  TODO: Add the current lap and time to the "laps" div.
-//     $("#laps").append("<p> Lap "+stopwatch.lap+" : "+ timeCurrent+"</p>")
-//     stopwatch.lap++;
-
-//     //  TODO: Increment lap by 1. Remember, we can't use "this" here.
-//   },
-
-//   count: function() {
-
-//     //  TODO: increment time by 1
-//     stopwatch.time++;
-//     var timeCurrent = stopwatch.timeConverter(stopwatch.time);
+ 
+  count: function() {
+    //  TODO: increment time by 1
+    stopwatch.time++;
     
-//     $("#display").text(timeCurrent);
+    var timeRemain = stopwatch.timeCountdown(stopwatch.time);
+    $("#timer").text(timeRemain);
 
-//     timeRemain = stopwatch.timeCountdown(stopwatch.time);
-//     $("#timer").text(timeRemain);
+  },
+
+  
+  timeCountdown: function(t) {
+    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+    var minutes = '00';
+    var seconds = 7-t;
+    if (seconds<0){
+      stopwatch.stop();
+      seconds = "00";
+    }
+     seconds = "0" + seconds;
+  
+    return minutes + ":" + seconds;
+  }
+
+}
 
 
-//     //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
-//     //        and save the result in a variable.
+$("#start").click(startButtn);
+$("#start").click(stopwatch.start)
+$("button").click(clickonButn);
+$(".answerButton").click(buttonValue);
 
-//     //  TODO: Use the variable you just created to show the converted time in the "display" div.
-
-//   },
-
-//   //  THIS FUNCTION IS DONE FOR US!
-//   //  We do not need to touch it.
-
-//   timeConverter: function(t) {
-
-//     //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-//     var minutes = Math.floor(t / 60);
-//     var seconds = t - (minutes * 60);
-
-//     if (seconds < 10) {
-//       seconds = "0" + seconds;
-//     }
-
-//     if (minutes === 0) {
-//       minutes = "00";
-//     }
-
-//     else if (minutes < 10) {
-//       minutes = "0" + minutes;
-//     }
-
-//     return minutes + ":" + seconds;
-//   },
-
-//   timeCountdown: function(t) {
-
-//     //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-//     var minutes = 14- Math.floor(t / 60);
-//     var seconds = 10- (t - (minutes * 60));
-    
-//     if (seconds < 10) {
-//       seconds = "5" + seconds;
-//     }
-
-//     if (minutes === 0) {
-//       minutes = "14";
-//     }
-
-//     else if (minutes < 10) {
-//       minutes = "0" + minutes;
-//     }
-
-//     return minutes + ":" + seconds;
-//   }
-
+});
